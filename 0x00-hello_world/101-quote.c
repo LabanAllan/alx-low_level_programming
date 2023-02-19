5e55df85f9d9ad460db588c4522f7bf47f7065b1
LabanAllan <labanallan254@gmail.com>
2023-02-19 11:02:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - Entry point
+ * Description : 'using puts function'
+ * Return: Always (0)(Success)
+ */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
