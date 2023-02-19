16d6c7a8999354b14858aac7a76b9f991b580942
LabanAllan <labanallan254@gmail.com>
2023-02-19 08:27:21 -0800

    computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  * Description : 'using puts function'
  * Return: Always (0)(Success)
  */
-int main(void)
+	int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-printf("the size of char is :%lu byte(s)\n", (unsigned long)sizeof(a));
-printf("the size of int is :%lu byte(s)\n", (unsigned long)sizeof(b));
-printf("the size of long int is :%lu byte(s)\n", (unsigned long)sizeof(c));
-printf("the size of long long int is :%lu byte(s)\n", (unsigned long)sizeof(d));
-printf("the size of float is :%lu byte(s)\n", (unsigned long)sizeof(f));
+		char a;
+		int b;
+		long int c;
+		long long int d;
+		float f;
+	printf("the size of char is :%lu byte(s)\n", (unsigned long)sizeof( char));
+	printf("the size of int is :%lu byte(s)\n", (unsigned long)sizeof( int));
+	printf("the size of long int is :%lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("the size of long long int is :%lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("the size of float is :%lu byte(s)\n", (unsigned long)sizeof( float));
 return (0);
 }
