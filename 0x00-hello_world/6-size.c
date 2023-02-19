f82f846b54ecb6e0ae1a2ba218ac4c16c94490a5
LabanAllan <labanallan254@gmail.com>
2023-02-19 10:34:19 -0800

    computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@
 		long int c;
 		long long int d;
 		float f;
-	printf("the size of char is :%zu byte(s)\n", sizeof( char));
-	printf("the size of int is :%zu byte(s)\n", sizeof( int));
-	printf("the size of long int is :%zu byte(s)\n", sizeof(long int));
-	printf("the size of long long int is :%zu byte(s)\n", sizeof(long long int));
-	printf("the size of float is :%zu byte(s)\n", sizeof( float));
+	printf("the size of char is: %zu byte(s)\n", sizeof( char));
+	printf("the size of int is: %zu byte(s)\n", sizeof( int));
+	printf("the size of long int is: %zu byte(s)\n", sizeof(long int));
+	printf("the size of long long int is: %zu byte(s)\n", sizeof(long long int));
+	printf("the size of float is: %zu byte(s)\n", sizeof( float));
 return (0);
 }
