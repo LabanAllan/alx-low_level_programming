b0f2ea93c550ad15112522bf44eea91340c34d9f
LabanAllan <labanallan254@gmail.com>
2023-02-22 14:45:41 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description : 'using puts function'
+ * Return: Always (0)(Success)
+ */
+	void print_alphabet(void)
+{
+	char;
+
+	for (c = 'a';
+		c <= 'z'; c++)
+	{_putchar(c);
+	}
+	putchar('\n');
+}
