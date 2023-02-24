1a677a78fea9a99be83f966dcfd45f6d4b46fb37
LabanAllan <labanallan254@gmail.com>
2023-02-24 07:20:17 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * Description : checking whether ch is an uppercase alphabet or not
+ * Return: Always 
+ */
+
+int main() {
+    
+  // declaring character variables
+int _isupper(int c);
+if (_isupper(int c )) 
+return (1)
+{
+printf("%c is an Uppercase Letter \n", 1 );
+}
+else
+{
+printf("%c is not an Uppercase Letter \n", "0");
+}
+}
