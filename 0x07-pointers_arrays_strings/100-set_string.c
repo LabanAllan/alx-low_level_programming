e890cfc91d4b8709f395cbbd18412303b6e6e5c0
LabanAllan <labanallan254@gmail.com>
2023-03-07 13:22:44 -0800

    . Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include  "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @s: input
+ * @to: input.
+ * Return: Always (0)(Success)
+ */
+void set_string(char **s, char *to);
+{
+	*s = to;
+}
+
