b03a0d8cd14941d7c63a8684bf2c1d0300802c94
LabanAllan <labanallan254@gmail.com>
2023-04-03 15:54:11 -0700

    updated

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <"lists.h">
+
+struct listint_t
+{
+	struct *head;
+	struct *new;
+	struct hello = {8, NULL};
+
+	int n
+};
+
+int main(void)
+
+{
+	/*initializing struct*/
+	int *head, *new, n;
+
+
+}
+
+	
