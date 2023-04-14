47dbc391afbcfef608b032a3aa78ae7685edab99
LabanAllan <labanallan254@gmail.com>
2023-04-14 13:14:19 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 void error_file(int file_from,  int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", agrv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 	}
 	if (file_to  == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", agrv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
 			exit (99);
 	}
 }
